43c290035f39a11f5af5f20a336afab34a161d2d
Dan Brooks <dmbrooks@live.co.uk>
2017-06-30 14:08:07 +0100

    Initial commit to generate maze

--- src/Main.elm
+++ src/Main.elm
@@ -1,49 +1,38 @@
 module Main exposing (..)
 
+import List exposing (map)
 import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Html.Attributes exposing (src, class)
 
-
----- MODEL ----
-
-
-type alias Model =
-    {}
-
+type alias Model = {}
 
 init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init = ( {}, Cmd.none )
 
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
+type Msg = NoOp
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update msg model = ( model, Cmd.none )
 
+type Slot = X | O
 
+renderSlot : Slot -> Html Msg
+renderSlot slot = case slot of
+    X -> div [class "maze__grid maze__grid--blocked"] []
+    O -> div [class "maze__grid"] []
 
----- VIEW ----
-
+row : List (Slot) -> Html Msg
+row els = div [class "maze__row"] (map renderSlot els)
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , div [] [ text "Your Elm App is working!" ]
+    div [ class "maze" ]
+        [ row [ X, O, X, X, X ]
+        , row [ X, O, O, O, X ]
+        , row [ X, O, X, O, X ]
+        , row [ X, O, X, O, X ]
+        , row [ X, X, X, O, X ]
         ]
-
-
-
----- PROGRAM ----
-
 
 main : Program Never Model Msg
 main =
