57f7c38ed916a306f74475db0226d39268a50802
Dan Brooks <dmbrooks@live.co.uk>
2017-07-03 17:17:08 +0100

    Get some procedural generation going

--- src/Main.elm
+++ src/Main.elm
@@ -17,24 +17,60 @@ update msg model = ( model, Cmd.none )
 type Slot = X | O
 type alias Row = List Slot
 type alias Point = { x : Int, y : Int }
-type alias Maze = List (Row)
+type alias Maze = List Row
 
-carve : Int -> Int -> List Point -> List Point
-carve x y state =
-  if length state == 0 then
-    [ { x = 1, y = 0 }
-    , { x = x - 2, y = y - 1 }
-    ]
-  else
-    state
+carve : Int -> Int -> Point -> List Point -> List Point
+carve x y current state =
+  let
+    next = flatten [[current], state]
+    rec = carve x y
+    isValid = validSlot x y
+  in
+    if length state == 0 || isValid current then
+      current
+      |> availableSlots x y next
+      |> foldr rec next
+    else
+      []
+
+availableSlots : Int -> Int -> List Point -> Point -> List Point
+availableSlots x y state current =
+  let
+    isValid = validSlot x y
+    isMember = (\pt -> member pt state)
+  in
+    current
+    |> neighbors
+    |> reject isMember
+    |> filter isValid
+
+validSlot : Int -> Int -> Point -> Bool
+validSlot x y pt = (pt.x > 0 && pt.x < (x - 1) && pt.y > 0 && pt.y < (y - 1))
+
+reject : (a -> Bool) -> List a -> List a
+reject f xs = filter (\x -> f x == False) xs
+
+neighbors : Point -> List Point
+neighbors pt =
+  [ { x = pt.x - 1, y = pt.y }
+  , { x = pt.x + 1, y = pt.y }
+  , { x = pt.x,     y = pt.y - 1 }
+  , { x = pt.x,     y = pt.y + 1 }
+  ]
+
+flatten : List (List a) -> List a
+flatten xs =
+  case xs of
+    [] -> []
+    h :: t -> concat [ h, (reject (\x -> member x h) <| flatten t) ]
 
 generateMaze : Int -> Int -> List (Html Msg)
 generateMaze x y =
   let
-    spaces = carve x y []
-    emptyMaze = repeat y (repeat x X)
+    spaces = carve x y { x = 1, y = 0 } []
+    emptyMaze = repeat y <| repeat x X
   in
-    map row (evaluateMaze emptyMaze spaces)
+    map row <| evaluateMaze emptyMaze spaces
 
 evaluateMaze : List Row -> List Point -> List Row
 evaluateMaze maze paths = indexedMap (evaluateRows paths) maze
@@ -47,10 +83,10 @@ evaluateCol paths y x slot =
   if member { x = x, y = y } paths then O else slot
 
 view : Model -> Html Msg
-view model = div [ class "maze" ] (generateMaze 5 5)
+view model = div [ class "maze" ] <| generateMaze 10 10
 
 row : List (Slot) -> Html Msg
-row els = div [class "maze__row"] (map renderSlot els)
+row els = div [class "maze__row"] <| map renderSlot els
 
 renderSlot : Slot -> Html Msg
 renderSlot slot = case slot of
